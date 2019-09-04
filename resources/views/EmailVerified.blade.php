246fa9848ab581b459d4f9bc7f960987dfa41170
ferimirpan <ferimirpan@gmail.com>
2019-09-04 16:30:37 +0700

    add view verified  page responsive

--- resources/views/EmailVerified.blade.php
+++ resources/views/EmailVerified.blade.php
@@ -14,35 +14,25 @@
 
 <body>
     <center>
-
     <!-- BANNER -->
     <div class="banner">
-    <img src="https://1.bp.blogspot.com/-1vokX6HMcFI/XW4lsu1_96I/AAAAAAAAAko/3oDOwXZYItchxoEHAMBHRyLu0eyhf-9rACLcBGAs/s1600/logo_mbaku.png" class="my-img" alt="Responsive image">
+        <img src="https://1.bp.blogspot.com/-1vokX6HMcFI/XW4lsu1_96I/AAAAAAAAAko/3oDOwXZYItchxoEHAMBHRyLu0eyhf-9rACLcBGAs/s1600/logo_mbaku.png" class="my-img" alt="Responsive image">
     </div>
-<!-- end baner -->
+    <!-- end baner -->
 
 
     <div class="container">
         <h1>Verifikasi Email Berhasil</h1><br>
         <p>Selamat Verifikasi E-mail Anda telah berhasil. silahkan buka aplikasi Mbaku kembali untuk dapat melakukan transaksi</p>
+    </div>
+
+    <div class="footer">
         <p>&copy;2019 Mbaku. All rights reserved.</p>
     </div>
 
-    
-  
 </center>
 
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>

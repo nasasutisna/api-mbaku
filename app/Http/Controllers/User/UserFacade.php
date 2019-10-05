b020ffb5ae2ee120944eaee0125bc8d4ad8cdfb5
ferimirpan <ferimirpan@gmail.com>
2019-10-05 21:12:23 +0700

    rename response variable isAlreadyAlertTrans

--- app/Http/Controllers/User/UserFacade.php
+++ app/Http/Controllers/User/UserFacade.php
@@ -25,7 +25,7 @@ class UserFacade
     }
 
     private function getUserInfoData($memberID) {
-        return DB::table(DB::raw('(select a.*, b.createdDt transDt, b.isAlreadyAlert isNewTrans from member a 
+        return DB::table(DB::raw('(select a.*, b.createdDt transDt, b.isAlreadyAlert isAlreadyAlertTrans from member a 
             left join (select * from transaction_loan order by createdDt desc limit 1) b on a.memberID=b.memberID
             left join member_premium c on a.memberID=c.memberID) x'))->where('memberID', '=', $memberID)->first();
     }
